9b7a093cc58c82f481eb4bb96d54b31d10fa9bbc
Amy Kapernick <amy@kapers.dev>
2020-05-22 18:10:32 +0800

    added link to graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 const IndexPage = (data) => {
-	const { content } = data.data.landingPagesJson;
-
+	console.log(data)
 	return (
-		<h1>Index Page</h1>
+		<div>
+			<h1>Index Page</h1>
+		<a href="__graphql?query=%7B%0A%20%20landingPagesJson(title%3A%20%7Beq%3A%20%22Home%22%7D)%20%7B%0A%20%20%20%20title%0A%20%20%20%20content%20%7B%0A%20%20%20%20%20%20sections%20%7B%0A%20%20%20%20%20%20%20%20content%0A%20%20%20%20%20%20%20%20heading%0A%20%20%20%20%20%20%20%20contentHtml%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A">Access GraphQL</a>
+		</div>
 	);
 };
 
